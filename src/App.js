18a1e17ae4ff48a0abcc0d5dcf0b6fd7590a9900
john-franti <john.franti@flatironschool.com>
2019-10-21 10:59:20 -0700

    update comments in JobTracker class

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import JobTracker from './lib/JobTracker'
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import JobTracker from "./lib/JobTracker";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: {}
+  };
+
+  // This is set up to default drop all users into state. It is a basic test to confirm
+  // interaction. Change it before deployment.
+  // Note that Firebase Promises use the method toJSON(), instead of .json()
+  componentDidMount() {
+    JobTracker.getAllUsers()
+      .then(resp => resp.toJSON())
+      .then(data => this.setState({ users: data }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/lib/JobTracker.js
+++ src/lib/JobTracker.js
@@ -4,12 +4,17 @@ import User from "./src/User";
 // The JobTracker class contains a series of methods, each of which
 // returns a Promise. They should be used in standard async fashion
 // to initiate React component updates using standard Promise and async
-// `then()` expressions.
+// `then()` expressions. (use .toJSON() to parse)
 
 export default class JobTracker {
+  // Get a master list of all users (DO NOT USE, ADMIN and TESTING ONLY)
   static getAllUsers = () => IFirebaseUser.getUsers();
+
+  // Create a new user, needs 3 params
   static saveNewUser = (username, email, password) => {
     return IFirebaseUser.createUser(new User(username, email, password));
   };
+
+  // Recover a user by username
   static findUser = username => IFirebaseUser.getUserByUsername(username);
 }
